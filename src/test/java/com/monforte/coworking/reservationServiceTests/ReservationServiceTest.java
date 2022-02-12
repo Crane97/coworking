630a50ed62c21eefd84db441454e66e79c75c8f8
JorgeMaster <jorge@macdiego.com>
2022-02-12 20:55:56 +0100

    Test try

--- src/test/java/com/monforte/coworking/reservationServiceTests/ReservationServiceTest.java
+++ src/test/java/com/monforte/coworking/reservationServiceTests/ReservationServiceTest.java
@@ -1,12 +1,18 @@
 package com.monforte.coworking.reservationServiceTests;
 
+import com.monforte.coworking.domain.entities.Reservation;
+import com.monforte.coworking.services.impl.ReservationService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -15,12 +21,22 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 @ExtendWith(MockitoExtension.class)
 public class ReservationServiceTest {
 
+    @Mock
+    public ReservationService reservationService;
+
     @Test
     public void compareLocalDateTimesReservations_test(){
         LocalDateTime start1;
         LocalDateTime start2;
         LocalDateTime end1;
         LocalDateTime end2;
+
+        List<Reservation> mockList = new ArrayList<>();
+
+        Mockito
+                .doReturn(null)
+                .when(this.reservationService)
+                .getReservations();
 
         assertTrue(true);
         assertFalse(false);
