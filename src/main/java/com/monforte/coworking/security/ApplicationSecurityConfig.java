7a5da2afae15a6b8115dc5b7f2e61d8bf99af8cd
JorgeMaster <jorge@macdiego.com>
2022-02-22 08:50:03 +0100

    security

--- src/main/java/com/monforte/coworking/security/ApplicationSecurityConfig.java
+++ src/main/java/com/monforte/coworking/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.monforte.coworking.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.monforte.coworking.security.ApplicationUserPermission.USER_WRITE;
 import static com.monforte.coworking.security.ApplicationUserRole.*;
 
 @Configuration
@@ -32,6 +34,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*" ).permitAll()
                 .antMatchers("/api/**").hasRole(ADMIN.name())
+                .antMatchers(HttpMethod.DELETE, "/api/**").hasAuthority(USER_WRITE.getPermission())
+                .antMatchers(HttpMethod.POST, "/api/**").hasAuthority(USER_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT, "/api/**").hasAuthority(USER_WRITE.getPermission())
+                .antMatchers(HttpMethod.GET, "/api/**").hasAnyRole(PARTNER.name(), ADMIN.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -44,13 +50,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails jorgeUser = User.builder()
                 .username("jorge")
                 .password(passwordEncoder.encode("root"))
-                .roles(PARTNER.name()) //ROLE_PARTNER
+                //.roles(PARTNER.name()) //ROLE_PARTNER
+                .authorities(PARTNER.getGrantedAuthorities())
                 .build();
 
         UserDetails albertoUser = User.builder()
                 .username("alberto")
                 .password(passwordEncoder.encode("root"))
-                .roles(ADMIN.name()) //ROLE_ADMIN
+                //.roles(ADMIN.name()) //ROLE_ADMIN
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
         return new InMemoryUserDetailsManager(
--- src/main/java/com/monforte/coworking/security/ApplicationUserRole.java
+++ src/main/java/com/monforte/coworking/security/ApplicationUserRole.java
@@ -1,8 +1,11 @@
 package com.monforte.coworking.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.monforte.coworking.security.ApplicationUserPermission.*;
 
@@ -35,5 +38,17 @@ public enum ApplicationUserRole {
 
     ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
         this.permissions = permissions;
+    }
+
+    public Set<ApplicationUserPermission> getPermissions(){
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities(){
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return permissions;
     }
 }
