453c8b23890086ae5ea06fa4885e554888d28e13
JorgeMaster <jorge@macdiego.com>
2022-03-27 23:12:19 +0200

    UserResponseTO

---
+++ src/main/java/com/monforte/coworking/domain/dto/responses/UserResponseTO.java
@@ -0,0 +1,12 @@
+package com.monforte.coworking.domain.dto.responses;
+
+public class UserResponseTO {
+
+    private String name;
+    private String surname;
+    private Boolean publicable;
+    private Boolean partner;
+    private Boolean openToWork;
+    private String jobTitle;
+
+}
